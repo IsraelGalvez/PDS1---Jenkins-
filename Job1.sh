cb8406f0bb68002d850e2a0ef396d21cef8a1524
BuhM0 <35952736+BuhMo@users.noreply.github.com>
2022-04-07 12:34:11 -0700

    Update Job1.sh

--- Job1.sh
+++ Job1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #empezar el loop
-for a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+or a in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do 
 	#if es igual a 8 que el loop descanse
 	if [ $a == 8 ]
