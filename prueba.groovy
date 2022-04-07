a3280c70b0a6e5ee7aef9471f0d69861c8d7fbca
BuhM0 <35952736+BuhMo@users.noreply.github.com>
2022-04-07 13:04:32 -0700

    Update prueba.groovy

--- prueba.groovy
+++ prueba.groovy
@@ -1,26 +1,27 @@
-job('Job-1-hijo') 
+job('Job-1-hijo') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	steps {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      mailer('nakama_51@hotmail.com', true, true)
-      slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
+		mailer('nakamakey@hotmail.com', true, true)
+		mailer('pedroabmin01@gmail.com', true, true)
+		slackNotifier {
+			notifyAborted(true)
+			notifyEveryFailure(true)
+			notifyNotBuilt(false)
+			notifyUnstable(false)
+			notifyBackToNormal(true)
+			notifySuccess(false)
+			notifyRepeatedFailure(false)
+			startNotification(false)
+			includeTestSummary(false)
+			includeCustomMessage(false)
+			customMessage(null)
+			sendAs(null)
+			commitInfoChoice('NONE')
+			teamDomain(null)
+			authToken(null)
         	}
     	}
 }
